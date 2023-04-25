dac3a4494f8bc7bbe462d60ec822771c96b559bd
Bastian Gruber <foreach@me.com>
2023-04-25 09:55:51 +0200

    Adjust how to pass IP and PORT

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tracing::{debug, error, info};
 
+const IP_ADDRESS: &str = "0.0.0.0";
+const PORT: u16 = 6374;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let mut stream = TcpStream::connect("37.187.124.186:1222").await?;
+    let mut stream = TcpStream::connect(format!("{IP_ADDRESS}:{PORT}")).await?;
     let (mut read, mut write) = stream.split();
 
     let mut buf: [u8; 4] = [0; 4];
 
-    // Byte message for Problem #2 - Means to an End
+    // Byte message for protohackers.com problem #2 - "Means to an End""
     let message = [
         0x49, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x65, 0x49, 0x00, 0x00, 0x30, 0x3a, 0x00,
         0x00, 0x00, 0x66, 0x49, 0x00, 0x00, 0x30, 0x3b, 0x00, 0x00, 0x00, 0x64, 0x49, 0x00, 0x00,
